3381358b55a05de4c14edea921a2e16c8c4b1f57
MShahryiar <muhammadshahryiar@yahoo.com>
2022-06-11 20:30:51 +0500

    UI Fix for more Page

---
+++ components/MoreButton.js
@@ -0,0 +1,17 @@
+import {Text,TouchableOpacity } from 'react-native'
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+
+ const MoreButtonTop = (props) => {
+  return (
+    <TouchableOpacity style={{flex:1,maxHeight:50,backgroundColor:'white',padding:15,alignItems:'center',justifyContent:'space-between', flexDirection:'row'}}>
+    <Text style={{flex:1 , fontSize:13}}>{props.leftIcon}</Text>
+    <Text style={{flex:4 ,fontSize:13}}>{props.text}</Text>
+    <FontAwesome name={props.rightIcon} style={{fontSize:25,color:'gray'}}/>        
+  </TouchableOpacity>
+  )
+}
+
+export default MoreButtonTop;
+
+
--- screens/More.js
+++ screens/More.js
@@ -1,37 +1,27 @@
-import { View, Text, SafeAreaView,StatusBar,TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView,StatusBar } from 'react-native'
 import MenuHeader from '../components/MenuHeader'
+import MoreButtonTop from '../components/MoreButton'
+import MoreButtonBottom from '../components/MoreButton'
 
 const More = () => {
   return (
     <SafeAreaView style={{flex:1}}>
     <StatusBar/>
     <MenuHeader title='More'/>
-    <View style={{flex:6, backgroundColor:'lightgray'}}>
-    <View style={{width:'100%',flexDirection:'row',justifyContent:'space-evenly'}}>
-      <TouchableOpacity><View style={{backgroundColor:'white', justifyContent:'center',margin:10,padding:40 }}><Text style={{textAlign:'center'}}>My Orders</Text></View></TouchableOpacity>
-      <TouchableOpacity><View style={{backgroundColor:'white', justifyContent:'center',margin:10,padding:40, paddingHorizontal:65}}><Text style={{textAlign:'center'}}>My Profile</Text></View></TouchableOpacity>
-      </View>
-      <View style={{width:'100%',flexDirection:'row',justifyContent:'space-evenly'}}>
-      <TouchableOpacity><View style={{backgroundColor:'white', justifyContent:'center',margin:10,padding:40 , paddingHorizontal:80}}><Text style={{textAlign:'center'}}>Store Location</Text></View></TouchableOpacity>
-      <TouchableOpacity><View style={{backgroundColor:'white', justifyContent:'center',margin:10,padding:40, paddingHorizontal:80}}><Text style={{textAlign:'center'}}>Contact Us</Text></View></TouchableOpacity>
-      </View>
-      <View style={{width:'100%',flexDirection:'row',justifyContent:'space-evenly'}}>
-      <TouchableOpacity><View style={{backgroundColor:'white',justifyContent:'center',margin:10,padding:40 , paddingHorizontal:80}}><Text style={{textAlign:'center'}}>Privacy policy</Text></View></TouchableOpacity>
-      <TouchableOpacity><View style={{backgroundColor:'white', justifyContent:'center',margin:10,padding:40, paddingHorizontal:80}}><Text style={{textAlign:'center'}}>Change Password</Text></View></TouchableOpacity>
-      </View>
-      <View style={{width:'100%',flexDirection:'row',justifyContent:'space-evenly'}}>
-      <TouchableOpacity><View style={{backgroundColor:'white', justifyContent:'center',margin:10,padding:40 , paddingHorizontal:80}}><Text style={{textAlign:'center'}}>About Us</Text></View></TouchableOpacity>
-      <TouchableOpacity><View style={{backgroundColor:'white', justifyContent:'center',margin:10,padding:40, paddingHorizontal:80}}><Text style={{textAlign:'center'}}>Mitao Bhook</Text></View></TouchableOpacity>
-      </View>
-      <View style={{width:'100%',flexDirection:'row',justifyContent:'space-evenly'}}>
-      <TouchableOpacity><View style={{backgroundColor:'white', justifyContent:'center',margin:10,padding:40 , paddingHorizontal:80}}><Text style={{textAlign:'center'}}>Our Secret Recipe</Text></View></TouchableOpacity>
-      </View> 
-    </View>
-    <View style={{flex:1,backgroundColor:'lightgray', flexDirection:'row', justifyContent:'flex-end',alignItems:'center'}}>
-      <View style={{flexDirection:'row'}}>
-      <TouchableOpacity><View style={{backgroundColor:'red',width:150, borderRadius:50, justifyContent:'center',margin:10,padding:15}}><Text style={{textAlign:'center',color:'white'}}>Log out</Text></View></TouchableOpacity>
-      </View>       
+    <View style={{flex:1, backgroundColor:'lightgray'}}>
+            <MoreButtonTop text="My Orders" leftIcon="icon" rightIcon="angle-right"/>
+            <MoreButtonTop text="My Profile"  leftIcon="icon" rightIcon="angle-right"/>
+            <MoreButtonTop text="Store Location" leftIcon="icon" rightIcon="angle-right"/>
+            <MoreButtonTop text="Contact us"  leftIcon="icon" rightIcon="angle-right"/>
+            <MoreButtonTop text="Change password" leftIcon="icon" rightIcon="angle-right"/>
+            <MoreButtonTop text="About Us" leftIcon="icon"/>
+            <MoreButtonTop text="Mithao Bhook" leftIcon="icon"/>
+            <MoreButtonTop text="Our secret recipe" leftIcon="icon"/>
+            <MoreButtonTop text="Privacy policy" leftIcon="icon"/>
+            <MoreButtonTop text="Terms and conditions" leftIcon="icon"/>
+            <MoreButtonTop text="Logout" leftIcon="icon"/>
+         
+            
     </View>
     
 </SafeAreaView>
